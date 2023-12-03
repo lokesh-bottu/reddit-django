254edce15b190c9d235cb5b5f4fea294da601a8d
lokesh-bottu <lokeshbottu1122@gmail.com>
2023-12-03 20:58:45 +0530

    comments implemented not working

--- UserProfile/forms.py
+++ UserProfile/forms.py
@@ -3,3 +3,7 @@ from django import forms
 
 class LikeForm(forms.Form):
     post_id = forms.IntegerField()
+
+
+class CommentForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea)
--- UserProfile/views.py
+++ UserProfile/views.py
@@ -1,11 +1,14 @@
 
 from django.shortcuts import  redirect, render
 from django.views.decorators.csrf import csrf_exempt
+from django.template.loader import render_to_string
+
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse,JsonResponse
-from .models import Post
+from .models import Post,Comment
 from django.contrib.auth.models import User
-from .forms import LikeForm
+from .forms import LikeForm,CommentForm
 import json
 
 def create_post_view(request):
@@ -63,4 +66,38 @@ def like_post(request):
         except json.JSONDecodeError:
             print("Invalid JSON data")
     print("Invalid request")
+    return JsonResponse({'error': 'Invalid request'})
+
+
+
+
+
+
+
+
+
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = Comment.objects.filter(post_id=post_id)
+    comment_form = CommentForm()
+
+    return render(request, 'index1.html', {'post': post, 'comments': comments, 'comment_form': comment_form})
+
+def comment_post(request):
+    if request.method == 'POST' and request.is_ajax():
+        post_id = request.POST.get('post_id')
+        text = request.POST.get('text')
+
+        post = get_object_or_404(Post, id=post_id)
+        user = request.user  # Assuming you have authentication in place
+
+        comment = Comment(user=user, post_id=post, text=text)
+        comment.save()
+
+        comments = Comment.objects.filter(post_id=post_id)
+        comments_html = render_to_string('comments_section.html', {'comments': comments})
+        
+        return JsonResponse({'comments_html': comments_html})
+
     return JsonResponse({'error': 'Invalid request'})
